14e464a048835965290ca715be2ff9f65341af5f
zkron <cnantais@gmail.com>
2026-05-02 18:25:42 -0400

    fixed Wishlist: Copy to Clipboard doesn't copy the URL to clipboard.

--- app/javascript/controllers/clipboard_controller.js
+++ app/javascript/controllers/clipboard_controller.js
@@ -1,7 +1,7 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  statis values = { test: String }
+  static values = { text: String }
 
   copy() {
     navigator.clipboard.writeText(this.textValue)
